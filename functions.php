681e1371d6632a3cc77d63b2a780a1ab8f384fab
Karsten Wachholz <k.wachholz@eniky.com>
2022-04-19 11:25:41 +0200

    Childtheme das genutzt wird ist Accessibility

--- functions.php
+++ functions.php
@@ -1,11 +1,30 @@
-<?php
+<?php /*
 
-// Defines
-define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
-define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+  This file is part of a child theme called Accessibility.
+  Functions in this file will be loaded before the parent theme's functions.
+  For more information, please read
+  https://developer.wordpress.org/themes/advanced-topics/child-themes/
 
-// Classes
-require_once 'classes/class-fl-child-theme.php';
+*/
 
-// Actions
-add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+// this code loads the parent's stylesheet (leave it in place unless you know what you're doing)
+
+function your_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, 
+      get_template_directory_uri() . '/style.css'); 
+
+    wp_enqueue_style( 'child-style', 
+      get_stylesheet_directory_uri() . '/style.css', 
+      array($parent_style), 
+      wp_get_theme()->get('Version') 
+    );
+}
+
+add_action('wp_enqueue_scripts', 'your_theme_enqueue_styles');
+
+/*  Add your own functions below this line.
+    ======================================== */ 
+
